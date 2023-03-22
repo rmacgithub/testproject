89e1aeab056ae039e99b0a522c3f2fff85450b68
Rob Mac <rtmemcfar@gmail.com>
2023-03-22 16:07:17 -0500

    first commit

---
+++ TestProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rtmem
+ *
+ */
+module TestProject {
+}
---
+++ TestProject/src/mysource/TestProject.java
@@ -0,0 +1,9 @@
+package mysource;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+	
+}
